c117452d6d4cb11d4f9f0818114827302f49d9e8
NahuelSalomon <nahuelmdp@hotmail.com>
2022-01-22 21:23:08 -0300

    Add controllers about Subject, SubjectStatics, User

--- src/main/java/com/example/UCESAPI/controller/SubjectController.java
+++ src/main/java/com/example/UCESAPI/controller/SubjectController.java
@@ -1,9 +1,56 @@
 package com.example.UCESAPI.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.UCESAPI.exception.SubjectNotExistsException;
+import com.example.UCESAPI.model.Subject;
+import com.example.UCESAPI.service.SubjectService;
+import com.example.UCESAPI.utils.EntityResponse;
+import com.example.UCESAPI.utils.EntityURLBuilder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/subject")
 public class SubjectController {
+
+    private final SubjectService subjectService;
+    private final String SUBJECT_PATH = "subject";
+
+    public SubjectController(SubjectService subjectService) {
+        this.subjectService = subjectService;
+    }
+
+    @PostMapping(value = "/")
+    public ResponseEntity<Object> add(@RequestBody Subject subject) {
+        Subject subjectCreated = this.subjectService.add(subject);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .location(EntityURLBuilder.buildURL(SUBJECT_PATH, subjectCreated.getId()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .build();
+    }
+
+    @GetMapping(value = "/")
+    public ResponseEntity<List<Subject>> getAll(@RequestParam(value = "size", defaultValue = "10") Integer size, @RequestParam(value ="page", defaultValue = "0") Integer page) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Subject> subjectPage = this.subjectService.getAll(pageable);
+        return EntityResponse.listResponse(subjectPage);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Subject> getById(@PathVariable Integer id) throws SubjectNotExistsException {
+        return ResponseEntity.ok(this.subjectService.getById(id));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Object> deleteById(@PathVariable Integer id) {
+        this.subjectService.deleteById(id);
+        return ResponseEntity.accepted().build();
+    }
+
 }
--- src/main/java/com/example/UCESAPI/controller/UserController.java
+++ src/main/java/com/example/UCESAPI/controller/UserController.java
@@ -1,9 +1,59 @@
 package com.example.UCESAPI.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.UCESAPI.exception.UserNotExistsException;
+import com.example.UCESAPI.model.User;
+import com.example.UCESAPI.service.UserService;
+import com.example.UCESAPI.utils.EntityResponse;
+import com.example.UCESAPI.utils.EntityURLBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+
+    private final UserService userService;
+    private final String USER_PATH = "user";
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<Object> add(@RequestBody User user) {
+        User userCreated = this.userService.add(user);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .location(EntityURLBuilder.buildURL(USER_PATH, userCreated.getId()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .build();
+    }
+
+    @GetMapping(value = "/")
+    public ResponseEntity<List<User>> getAll(@RequestParam(value = "size", defaultValue = "10") Integer size, @RequestParam(value ="page", defaultValue = "0") Integer page) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<User> userPage = this.userService.getAll(pageable);
+        return EntityResponse.listResponse(userPage);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> getById(@PathVariable Integer id) throws UserNotExistsException {
+        User user = this.userService.getById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Object> deleteById(@PathVariable Integer id) {
+        this.userService.deleteById(id);
+        return ResponseEntity.accepted().build();
+    }
+
 }
